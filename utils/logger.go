c7af46c155eeb467a9a5d8b4d0626e9b0eed7a2f
cedric lamoriniere <cedric.lamoriniere@amadeus.com>
2017-09-25 08:35:00 +0200

    Step3: implement missing logger part

--- utils/logger.go
+++ utils/logger.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"os"
+	"time"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/bshuster-repo/logrus-logstash-hook"
-	"time"
 )
 
 const (
@@ -16,21 +18,29 @@ const (
 // InitLog initializes the logrus logger
 func InitLog(logLevel, formatter string) error {
 
-	// TODO depending on formatter param, initialize the logrus logger as the logstash one
 	switch formatter {
 	case LogStashFormatter:
 		logrus.SetFormatter(&logrus_logstash.LogstashFormatter{
 			TimestampFormat: time.RFC3339,
 		})
 	default:
-		// TODO set the TextFormatter with forced color in output and full timestamp
+		logrus.SetFormatter(&logrus.TextFormatter{
+			ForceColors:   true,
+			FullTimestamp: true,
+		})
 	}
 
-	// TODO set the logger output to os.Stdout instead of os.Stderr by default
+	// set the logger output to os.Stdout instead of os.Stderr by default
+	logrus.SetOutput(os.Stdout)
 
-	// TODO parse the logLevel attribute
+	// parse the logLevel attribute
 	// in case of error set the logger level to logrus.DebugLevel and return the error
-
-	// TODO if ok set the parsed level to the logger
+	// if ok set the parsed level to the logger
+	level, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		logrus.SetLevel(logrus.DebugLevel)
+		return err
+	}
+	logrus.SetLevel(level)
 	return nil
 }
